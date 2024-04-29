b9b21db69227dee3d01aecd4bc1cb5680dcb2342
foxy12112 <Leon.dick1@gmx.de>
2024-04-29 13:33:21 +0200

    m

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 15:15:28 by ldick             #+#    #+#             */
-/*   Updated: 2024/04/17 16:44:04 by ldick            ###   ########.fr       */
+/*   Updated: 2024/04/29 12:58:31 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,16 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+/**
+ * @brief reads from a a file
+ * reads a text until a new line is encountered
+ * @note reads one line at a time, using a while loop you can read throuh a text file line by line
+ * 
+ * @param[in] fd - the file descriptor from which to read from
+ * @return read line
+*/
 char	*get_next_line(int fd);
+
 char	*gnl_strchr(char *s, int c);
 char	*gnl_strjoin(char *s1, char *s2);
 size_t	gnl_strlen(char *s);
